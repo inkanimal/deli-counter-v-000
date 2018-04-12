0f38a4a04e57e843900c2b61bb27d355b76e49be
geoffrey woodcock <theatomproject@gmail.com>
2018-04-12 18:48:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(katz_deli) 
    if katz_deli.length >= 1 
      current_line = ["string"]
-     string = "The"
+     
      katz_deli.each_with_index do |name, index| 
      current_line.push("#{index + 1}. #{name}")
     end
