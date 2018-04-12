1beaa0b2bd548c2959d5acd36880472a045e4abc
geoffrey woodcock <theatomproject@gmail.com>
2018-04-12 18:03:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
    puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index| 
-      "The line is currently: #{name}"
+     puts "The line is currently: #{name}"
 end
 end
 
