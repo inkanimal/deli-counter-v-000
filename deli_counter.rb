f38b926f86ad1971fcea940b0528c63fc12e0399
geoffrey woodcock <theatomproject@gmail.com>
2018-04-12 19:19:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,13 @@
 def line(katz_deli) 
    if katz_deli.length >= 1 
      current_line = [] 
-     
      katz_deli.each_with_index do |name, index| 
      current_line.push("#{index + 1}. #{name} ")
-    end
+   end
      puts "The line is currently: #{current_line.join("")}"
    else
      puts "The line is currently empty."
-  #end
- end
+  end
 end
 
 def now_serving(katz_deli) 
