45791e22199c6145024f4b4f9fcb95ba32ff52d4
geoffrey woodcock <theatomproject@gmail.com>
2018-04-12 18:36:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli) 
-   if katz_deli.length > 1 
+   if katz_deli.length >= 1 
      current_line []
      katz_deli.each_with_index do |name, index| 
      current_line.push("The line is currently: #{index + 1}. #{name}")
